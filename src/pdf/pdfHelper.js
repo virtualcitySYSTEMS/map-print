65e618a61ad498f3dcf548a1ccb3669cf7f47e00
Jannes Bolling <jbolling@vc.systems>
2023-06-08 14:24:44 +0000

    use new exported default Logo

--- src/pdf/pdfHelper.js
+++ src/pdf/pdfHelper.js
@@ -1,33 +1,26 @@
-import defaultLogo from '@vcmap/ui/src/assets/logo.svg';
+import { VcsDefaultLogo } from '@vcmap/ui';
 import { svgToPng } from '../common/util.js';
 
 /**
  * Adds prefixes to mail, phone and fax information.
  * @param {import("@vcmap/ui").VcsUiApp} app VcsUiApp instance.
- * @param {import("@vcmap/ui").VcsPlugin} plugin The print plugin.
  * @param {import("../common/configManager").ContactInfo} contactInfo The contact informations.
  * @returns {import("../pdf/pdfCreator").TextWithHeader} The fomratted contact information to be printed on the pdf file.
  */
-export function formatContactInfo(app, plugin, contactInfo) {
+export function formatContactInfo(app, contactInfo) {
   return {
-    header: plugin.i18n[app.locale].print.pdf.content.contact.header,
+    header: app.vueI18n.t('print.pdf.content.contact.header'),
     text: Object.keys(contactInfo)
       .filter((key) => contactInfo[key])
       .map((key) => {
         const value = contactInfo[key];
         switch (key) {
           case 'mail':
-            return `${
-              plugin.i18n[app.locale].print.pdf.content.contact.mail
-            }: ${value}`;
+            return `${app.vueI18n.t('pdf.content.contact.mail')}: ${value}`;
           case 'phone':
-            return `${
-              plugin.i18n[app.locale].print.pdf.content.contact.phone
-            }: ${value}`;
+            return `${app.vueI18n.t('pdf.content.contact.phone')}: ${value}`;
           case 'fax':
-            return `${
-              plugin.i18n[app.locale].print.pdf.content.contact.fax
-            }: ${value}`;
+            return `${app.vueI18n.t('pdf.content.contact.fax')}: ${value}`;
           default:
             return value;
         }
@@ -43,7 +36,7 @@ export function formatContactInfo(app, plugin, contactInfo) {
 export async function getLogo(app) {
   const logo = new Image();
   const mapLogo = app.uiConfig?.config?.value?.logo;
-  const logoSrc = mapLogo || defaultLogo;
+  const logoSrc = mapLogo || VcsDefaultLogo;
   // .svg file extension, <svg tag OR image/svg as in base64 encoded svg files.
   if (/([.<]|%3C)svg/gi.test(logoSrc) || /image\/svg/i.test(logoSrc)) {
     // 120 px is the height of the logo in 300 dpi in portrait mode.
@@ -58,19 +51,17 @@ export async function getLogo(app) {
 /**
  * Get informations about the current map view.
  * @param {import("@vcmap/core").VcsApp} app The vcs ui app.
- * @param {import("@vcmap/ui").VcsPlugin} plugin The print plugin.
  * @returns {Promise<import("./pdfCreator").TextWithHeader>} Information about the map that can be displayed in mapInfo text element.
  */
-export async function getMapInfo(app, plugin) {
-  const coordinatesTitle =
-    plugin.i18n[app.locale].print.pdf.content.centerCoordinate;
+export async function getMapInfo(app) {
+  const coordinatesTitle = app.vueI18n.t('print.pdf.content.centerCoordinate');
   const viewpoint = await app.maps.activeMap.getViewpoint();
   const { groundPosition } = viewpoint;
   const coordinates = `${coordinatesTitle}: ${groundPosition[0].toFixed(
     4,
   )}, ${groundPosition[1].toFixed(4)}`;
   return {
-    header: plugin.i18n[app.locale].print.pdf.content.mapInfo,
+    header: app.vueI18n.t('print.pdf.content.mapInfo'),
     text: [
       coordinates,
       // can be extended by other informations like layers
